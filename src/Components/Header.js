695181d20f3780af1493cb242acc7d9da8f2dde1
Kaniel <alonetogether484@gmail.com>
2022-07-29 10:33:58 +0400

    First commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
-import log from '../images/logo.svg'
+import log from '../images/logo.svg';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+
 
 function Header() {
+
+  const [ burgerStatus, setBurgerStatus] = useState(false);
+
+  
+
   return (
     <Container>
       <a href="#">
@@ -11,13 +21,37 @@ function Header() {
 
       {/* Menu */}
       <Menu>
-        <p><a href="#">Model S</a></p>
-        <p><a href="#">Model 3</a></p>
-        <p><a href="#">Model X</a></p>
-        <p><a href="#">Model Y</a></p>
-        <p><a href="#">Solar Roof</a></p>
-        <p><a href="#">Solar Panel</a></p>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+        <a href="#">Solar Roof</a>
+        <a href="#">Solar Panel</a>
       </Menu>
+
+      <RightMenu>
+          <a href='#'>Shop</a>
+          <a href='#'>Tesla Account</a>
+          <CustomMenu onClick={() => setBurgerStatus(true)} />
+      </RightMenu>
+
+      <BurgerNav show={burgerStatus}>
+        <CloseWrap>
+          <CustomeClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrap>
+        <li><a href='#'>Existing Inventory</a></li>
+        <li><a href='#'>Used Inventory</a></li>
+        <li><a href='#'>Trade-In</a></li>
+        <li><a href='#'>Test Drive</a></li>
+        <li><a href='#'>Insurance</a></li>
+        <li><a href='#'>Cybertruck</a></li>
+        <li><a href='#'>Roadster</a></li>
+        <li><a href='#'>Semi</a></li>
+        <li><a href='#'>Charging</a></li>
+        <li><a href='#'>Powerwall</a></li>        
+  
+      </BurgerNav>
+
+      
     </Container>
   )
 }
@@ -29,12 +63,109 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
-  padding: 0 20px
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+
+  background-color:#b5b1ec;
+	box-shadow: 0 35px 55px rgba(0, 0, 0, 0.1);
+  opacity: 0.65;
+
+	@media(max-width: 400px){
+		min-height: 40px;
+		padding: 0 10px;
+		a{
+			img{
+				width: 75px;
+			}
+		}
 `
 
 const Menu = styled.div`
-  margin: 20px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  flex: 1;
+
+  a{
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;    
+  }
+  a:hover{
+      transition: 0.1s ease-in-out;
+      font-size: 14px;
+      text-transform: capitalize;
+    }
+
+  @media(max-width: 768px){
+    display: none;
+
+  }
+  
+`
+
+const RightMenu = styled.div`
+      display: flex;
+      align-items: center;
+      a{
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+      }
+      a:hover{
+      transform: 0.1s ease-in;
+      font-size: 14px;
+      text-transform: capitalize;
+    }
+
+`
+
+const CustomMenu = styled(MenuIcon)`
+      cursor: pointer;
+      
+`
+
+const BurgerNav = styled.div`
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      background: #b5b1ec;
+      width: 300px;
+      z-index: 16;
+      list-style: none;
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+      text-align: start;
+      transform: ${ props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+      transition: transform 0.2s ease-in;
+      li{
+        padding: 15px;
+        border: 1px solid rgba(0,0,0,0.1);
+        align-items: center;
+          a{
+            font-weight: 600px;
+          }
+          a:hover{
+            font-weight: bold;
+            -webkit-transform: scale(1.3);
+            -ms-transform: scale(1.3);
+            transform: scale(1.3);
+          }
+      }
+`
+
+
+const CustomeClose = styled(CloseIcon)`
+      cursor: pointer;
+`
+const CloseWrap = styled.div`
+      display: flex;
+      justify-content: flex-end;
 `
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-// import img from '../images/model-s.jpg';
 import arro from '../images/down-arrow.svg';
+import Fade from 'react-reveal/Fade';
 
 
 function Section({title, description, LeftBtnText, RightBtnText, backgroundImage}) {
   return (
     <Wrap bgImage={backgroundImage}>
         {/* Under the section component, there is a text, here it's */}
-        <ItemtText>
-            <h1>{ title }</h1>
-            <p>{ description }</p>
-        </ItemtText>
-
+        <Fade top>
+            <ItemText>
+                <h1>{ title }</h1>
+                <p>{ description }</p>
+            </ItemText>
+        </Fade>
         {/* Button */}
-        <Button>
+        <Fade top>
+         <Buttons>
             <ButtonGroup>
                 <LeftButton>
                     { LeftBtnText }
@@ -24,68 +26,79 @@ function Section({title, description, LeftBtnText, RightBtnText, backgroundImage
             {/* An arrow  */}
             {/* <DownArrow src='window.location.origin + "/images/down-arrow.svg"'/> */}
             <DownArrow src={arro} />
-
-        </Button>
+         </Buttons>
+        </Fade>
     </Wrap>
   )
 }
 
 export default Section;
 
-const Wrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    display: flex;
-    flex-direction: column;
-    justify-content:space-between; //for vertical lines
-    align-items: center;
-    background-image: ${props => `url("${props.bgImage}")`};   
+const Wrap = styled.div `
+	width: 100vw;
+	height: 100vh;
+	background-size: cover; /* set size to cover */
+	background-position: center; /* center the image */
+	background-repeat: no-repeat; /* dont repeat the background */
+
+	display: flex;
+	flex-direction: column; /* switch to vertical */
+	justify-content: space-between; /* horizontal alignment */
+	align-items: center; /* vertical alignment */
+
+	background-image: ${props => `url("${props.bgImage}")`};
 `
 
-const ItemtText = styled.div`
-    padding-top: 15vh;
-    text-align: center;
+const ItemText = styled.div `
+	padding-top: 15vh;
+	text-align: center;
 
 `
-const ButtonGroup = styled.div`
-    display: flex;
-    margin-bottom: 40px;
-    @media(max-width: 768px){
-        flex-direction: column;
-    }
+const ButtonGroup = styled.div `
+	display: flex;
+	margin-bottom: 30px;
+
+	@media (max-width: 768px){
+		flex-direction: column;
+	}
+
+`
+
+const LeftButton = styled.div `
+	background-color: rgba(23, 26, 32, 0.8);
+	height: 40px;
+	width: 256px;
+	color: white;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100px;
+	opacity: 0.85;
+	text-transform: uppercase;
+	font-size: 12px;
+	font-weight: 600;
+	cursor: pointer;
+	letter-spacing: 1px;
+	margin: 8px;
+    
     
 `
+const RightButton = styled(LeftButton) `
+	background-color: white;
+	opacity: 0.65;
+	color: black;	
+`
 
-const LeftButton = styled.div`
-    background-color: rgba(23,26,32,0.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100px;
-    opacity: 0.85;
-    text-transformation: uppercase;
-    font-size: 14px;
+
+const DownArrow = styled.img `
+	height: 40px;
+	animation: animationDown infinite 1.5s;
     cursor: pointer;
-    margin: 8px;
-
+	overflow-x: hidden;
 `
-
-const RightButton = styled(LeftButton)`
-    background: #D7D4D4;
-    opacity: 0.75;
-    color: #474A4E;
+const Buttons = styled.div `
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `
-
-const DownArrow = styled.img`
-    margin-top: 20px;
-    height: 40px;
-    animation: animateDown infinite 1.5s;
-    overflow-x: hidden;
-`
-const Button = styled.div``
